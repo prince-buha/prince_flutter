b724af4bc436c64fd6802ea7566530f817e154a7
prince buha <150029430+prince-buha@users.noreply.github.com>
2024-04-17 11:55:27 +0530

    first commit

--- dart/ch 2/lecture 3 dart/1.dart
+++ dart/ch 2/lecture 3 dart/1.dart
@@ -9,21 +9,24 @@ void main() {
   numEmployees = int.parse(stdin.readLineSync()!);
 
   for (int i = 0; i < numEmployees; i++) {
-    stdout.write("Enter name of employee ${i + 1}: ");
+    stdout.write("Enter id of employee $i: ");
+    int  id =int.parse( stdin.readLineSync()!);
+    stdout.write("Enter name of employee $i: ");
     String name = stdin.readLineSync()!;
-    stdout.write("Enter age of employee ${i + 1}: ");
+    stdout.write("Enter age of employee $i: ");
     int age = int.parse(stdin.readLineSync()!);
-    stdout.write("Enter salary of employee ${i + 1}: ");
+    stdout.write("Enter salary of employee $i + 1}: ");
     double salary = double.parse(stdin.readLineSync()!);
 
     employees.add(MapEntry<String, dynamic>('Employee ${i + 1}', {
+      'id' : id,
       'name': name,
       'age': age,
       'salary': salary,
     }));
   }
 
-  // Printing total employee information
+ 
   print("\nTotal Employee Information:");
   for (var entry in employees) {
     print("${entry.key}: ${entry.value}");
