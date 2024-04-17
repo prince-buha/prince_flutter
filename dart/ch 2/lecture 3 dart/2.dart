f6930cb8f63bc2fd654cade553cb27c96c9ae24d
prince buha <150029430+prince-buha@users.noreply.github.com>
2024-04-17 11:56:20 +0530

    first commit

--- dart/ch 2/lecture 3 dart/2.dart
+++ dart/ch 2/lecture 3 dart/2.dart
@@ -1,23 +1,16 @@
 import 'dart:io';
-
 void main() {
-  List<String> elements = [];
-
-  // Getting elements from user input
-  int numElements;
-  stdout.write("Enter the number of elements: ");
-  numElements = int.parse(stdin.readLineSync()!);
-
-  for (int i = 0; i < numElements; i++) {
-    stdout.write("Enter element ${i + 1}: ");
-    String element = stdin.readLineSync()!;
-    elements.add(element);
-  }
-
-  // Printing unique elements
-  Set<String> uniqueElements = elements.toSet();
-  print("\nUnique Elements:");
-  for (var element in uniqueElements) {
-    print(element);
-  }
+    List<String> myList = [];
+    stdout.write("Enter Number of elements you want to add in List : ");
+    int len = int.parse(stdin.readLineSync()!);
+    for(int i = 1 ;i <= len; i++){
+      stdout.write("Enter the String $i Value : ");
+      String str = stdin.readLineSync()!;
+      myList.add(str);
+      print("");
+    }
+    myList = myList.toSet().toList();
+    myList.forEach((element) {
+      print("$element");
+});
 }
