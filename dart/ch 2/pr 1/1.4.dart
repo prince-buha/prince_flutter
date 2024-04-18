e44e71e603766043a488a817b2b65fea74c40854
prince buha <150029430+prince-buha@users.noreply.github.com>
2024-04-18 20:06:15 +0530

    frist commit

--- dart/ch 2/pr 1/1.4.dart
+++ dart/ch 2/pr 1/1.4.dart
@@ -1,15 +1,39 @@
-void main() 
-{
- List a=[[1,2,3],[6,7,8],[9,10,11]];
- List b=[[12,13,14],[15,16,17],[18,19,20]];
- List sum=[[0,0,0],[0,0,0],[0,0,0]];
+/*
+  
+  write a dart progrem to addition of two matries of dimension
+*/
 
-int i,j;
- for( i=0;i<3;i++){
- for( j=0;j<3;j++){ 
- sum[i][j]=a[i][j]+b[i][j];
- } 
- print("$sum");
- }
 
+
+import 'dart:io';
+void main(){
+  List<List<int>> Array1 = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90],
+  ];
+    List<List<int>> Array2 = [
+    [90, 80, 70],
+    [60, 50, 40],
+    [30, 20, 10],
+  ];
+  print("\n Array 1 \n : ");
+  Array1.forEach((e) {
+    stdout.write("$e ");
+    print("");
+  });
+  print("\n Array 2 \n:");
+  Array2.forEach((e) {
+    stdout.write("$e ");
+    print("");
+  });
+  print("\nArray 1 + Array 2 :\n");
+  Array1.forEach((e) {
+    Array2.forEach((e) {
+      stdout.write("${e + e} ");
+      print("");
+     });
+  });
+  
 }
+
